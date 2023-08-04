e98076e9ceff6b5a746011c4c637519678be3548
rootnure <nur.diu.2791@gmail.com>
2023-08-05 02:15:28 +0600

    toUpperCase(), toLowerCase()

---
+++ case.js
@@ -0,0 +1,12 @@
+const userName = 'Rootnure';
+const userInput = 'rootNure';
+console.log(userName.toLowerCase());
+console.log(userName.toUpperCase());
+userInput.toLocaleLowerCase()
+userInput.toLocaleUpperCase()
+if(userName.toLowerCase() === userInput.toLowerCase()) {
+    console.log('valid user');
+}
+else {
+    console.log('invalid user');
+}
