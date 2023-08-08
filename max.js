164153aaeb1a673b1dcee346e726bdf89a224cc7
rootnure <nur.diu.2791@gmail.com>
2023-08-08 16:43:35 +0600

    Max in two or three numbers using if-else and Math.max()

---
+++ max.js
@@ -0,0 +1,9 @@
+//78 97 //84 74
+const jim = 78;
+const dela = 97;
+if(jim > dela) {
+    console.log('jim will get the cake');
+}
+else {
+    console.log('dela will get the cake');
+}
---
+++ max2.js
@@ -0,0 +1,24 @@
+//84 99 77 //69 97 99
+
+const jim = 84;
+const dela = 93;
+const chinku = 95;
+
+/* 
+if(jim > dela && jim > chinku) {
+    console.log('jim will get the cake this time');
+}
+else if (dela > jim && dela > chinku) {
+    console.log('dela will be granted the cake');
+}
+else {
+    console.log('Chinku will be the new cake winner');
+}
+ */
+
+// Homework 1: do this using if-else inside a function and return the name;
+// Homework 2: do this using Math.max() inside a function and print the name;
+
+// Homework 3: find the lowest using if-else inside a function and return the lowest value
+// Homework 4: find the lowest using Math.min() inside a function and return the lowest value
+
